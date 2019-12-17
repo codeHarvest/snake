7da13589af71a86b424ac5ac497f618a6cbdadb0
codeHarvest <madhusudanbhardwaj94@gmail.com>
2019-12-17 21:41:23 +0530

    sanke game setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  
+
+  componentDidMount() {
+    this.drawCanvas(); 
+  }
+  
+  drawCanvas(){
+    let canvas = this.refs.canvas.getContext('2d');
+  }
+
+  render(){
+    return (
+        <div className="App">
+          <canvas ref="canvas" width="500" height="500" style={{border:'2px solid black'}}></canvas>
+        </div>
+      );
+  }
 }
 
 export default App;

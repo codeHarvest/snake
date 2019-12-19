ffe9a20d4bf3dd14ea6a4d7120546ffcc6c0ed70
codeHarvest <madhusudanbhardwaj94@gmail.com>
2019-12-19 21:24:25 +0530

    moved variables in state

---
+++ snake.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+import './App.scss';
+
+    var foodList;
+    var snakeList;
+    
+    
+    var snakeBody = {
+      width: 20,
+      height:20,
+      color: 'green'
+    }
+    var snakeFood = {
+      width: 20,
+      height: 20,
+      color: 'orange'
+    }
+    
+
+class App extends Component{
+  
+
+  componentDidMount() {
+    let canvas = this.refs.canvas.getContext('2d');
+    canvas.font = '20px Calibri';
+
+    this.startGame();
+  }
+
+  startGame(){
+    snakeList = [
+      {x:220,y:200},
+      {x:210,y:200},
+      {x:200,y:200}
+    ];
+
+    foodList = [];
+
+    snakeList.forEach(this.drawSnake());
+  }
+
+
+  drawSnake(sb,i){
+    this.canvas.save();
+    this.canvas.fillStyle = snakeBody.color;
+    this.canvas.fillRect(sb.x,sb.y,snakeBody.width,snakeBody.height);
+    this.canvas.restore();
+  }
+
+  drawFood(f,i){
+    this.canvas.save();
+    this.canvas.fillStyle = snakeFood.color;
+    this.canvas.fillRect(f.x,f.y,snakeFood.width,snakeFood.height);
+    this.canvas.restore();
+  }
+
+  render(){
+    return (
+        <div className="App">
+          <canvas ref="canvas" width="500" height="500" style={{border:'2px solid black'}}></canvas>
+        </div>
+      );
+  }
+}
+
+export default App;
--- src/App.js
+++ src/App.js
@@ -3,22 +3,63 @@ import './App.scss';
 
 class App extends Component{
   
+  constructor(){
+    super();
 
-  componentDidMount() {
-    this.drawCanvas(); 
+    this.state = {
+      snakeBody : {
+        width: 20,
+        height:20,
+        color: 'green'
+      },
+      Food: {
+        width: 20,
+        height: 20,
+        color: 'orange'
+      },
+      foodList: [],
+      snakeList:[]
+    }
   }
   
-  drawCanvas(){
+  componentDidMount() {
     let canvas = this.refs.canvas.getContext('2d');
+    canvas.font = '20px Calibri';
+    this.startGame();    
+  }
+
+
+  startGame(){
+    this.setState({snakeList: [
+      {x:220,y:200},
+      {x:210,y:200},
+      {x:200,y:200}
+    ]});
+
+    this.setState({
+      foodList: []
+    })
+
+    this.state.snakeList.forEach(this.drawSnake());
+  }
+
+  drawSnake(sb,i){
+    this.canvas.save();
+    this.canvas.fillStyle = this.state.snakeBody.color;
+    this.canvas.fillRect(sb.x,sb.y,this.state.snakeBody.width,this.state.snakeBody.height);
+    this.canvas.restore();
   }
 
   render(){
-    return (
-        <div className="App">
+  
+    return(
+      <div className="App">
           <canvas ref="canvas" width="500" height="500" style={{border:'2px solid black'}}></canvas>
-        </div>
-      );
+      </div>
+    )
+
   }
 }
+
 
 export default App;
